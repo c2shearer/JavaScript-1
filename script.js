fa0a8b9c754b33894654dfed4fa33913353f043a
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-01-04 10:06:39 +0000

    Updated JS file

--- script.js
+++ script.js
@@ -3,7 +3,50 @@ let cameron = "Cameron";
 let age = 19;
 
 for (let i = 0; i < cameron.length; i++) {
-    console.log(cameron[i])
+    console.log(cameron[i]);
 }
 
-console.log(`My name is ${cameron} and I am ${age}`)
+console.log(`My name is ${cameron} and I am ${age}`);
+
+// Opens Alert 
+// alert("Hello World!"); 
+
+// Prompts allow to capture information from a user
+// var username = prompt("Enter Username:");
+// alert(`Welcome to the Club, ${username}!`);
+
+// Calculator with prompts and alerts
+
+/*alert("Calculator");
+var x;
+while (x != "q") {
+    x = prompt("Add, Subtract, Multiply or Divide");
+    x.toLowerCase();
+    var num1, num2;
+    if (x == "add") {
+        num1 = prompt("First Number");
+        num2 = prompt("Second Number");
+        alert(`Answer: ${num1+num2}`);
+    }
+    else if (x == "subtract"){
+        num1 = prompt("First Number");
+        num2 = prompt("Second Number");
+        alert(`Answer: ${num1-num2}`);
+    }
+    else if (x == "multiply") {
+        num1 = prompt("First Number");
+        num2 = prompt("Second Number");
+        alert(`Answer: ${num1*num2}`);
+    }
+    else if (x == "divide") {
+        num1 = prompt("First Number");
+        num2 = prompt("Second Number");
+        alert(`Answer: ${num1/num2}`);
+    } 
+    else if (x == "q") {
+        alert("OKAY");
+    }
+    else {
+        alert("Not Known!");
+    }
+}*/
