9513268aeefa19508ee35f10f002083d501322cd
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-01-04 09:52:09 +0000

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Variable of 'Cameron'
+let cameron = "Cameron";
+let age = 19;
+
+for (let i = 0; i < cameron.length; i++) {
+    console.log(cameron[i])
+}
+
+console.log(`My name is ${cameron} and I am ${age}`)
