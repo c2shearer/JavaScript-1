8eb2994a322291bcbcc569b878eea52f8ef7fc23
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-01-11 10:28:17 +0000

    Ready for Fonts

--- new script.js
+++ new script.js
@@ -1,15 +1,21 @@
 function calculateCost() {
+    var error = document.getElementById("error").innerText = " ";
     var sign = document.getElementById("inputSign").value;
     var shipping = document.getElementById("shipping");
 
-    if (sign.length >= 15) {
+
+
+    if (sign.length >= 15 && sign.length < 30) {
         shipping.innerHTML = sign.length * (2.99 * 3);
+    }
+    else if (sign.length >= 30){
+        var error = document.getElementById("error").innerText = "Moron, you can't have a sign with 30 characters!";
     } else {
         shipping.innerHTML = sign.length * 2.99;
     }
 
-    var subtotal = document.getElementById("subtotal").innerHTML = sign.length * 2.99;
-    var signTotal = Math.round(subtotal + shipping.innerHTML);
+    var subtotal = document.getElementById("subtotal").innerHTML = sign.length * 1.99;
+    var signTotal = Math.round(subtotal + document.getElementById("shipping").value);
     document.getElementById("usersign").innerHTML = sign;
     document.getElementById("tiles").innerHTML = sign.length;
     document.getElementById("total").innerHTML = signTotal;
