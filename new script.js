5b9fb7f1fdc14f1bec8fb6bfc419d2716b268e97
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-01-11 10:07:47 +0000

    Added JavaScript

--- new script.js
+++ new script.js
@@ -1,14 +1,31 @@
-alert("hello");
+function calculateCost() {
+    var sign = document.getElementById("inputSign").value;
+    var shipping = document.getElementById("shipping");
 
-//invoice example
+    if (sign.length >= 15) {
+        shipping.innerHTML = sign.length * (2.99 * 3);
+    } else {
+        shipping.innerHTML = sign.length * 2.99;
+    }
+
+    var subtotal = document.getElementById("subtotal").innerHTML = sign.length * 2.99;
+    var signTotal = Math.round(subtotal + shipping.innerHTML);
+    document.getElementById("usersign").innerHTML = sign;
+    document.getElementById("tiles").innerHTML = sign.length;
+    document.getElementById("total").innerHTML = signTotal;
+}
+
+
+/*invoice example
 var sign = "Bristol Zoo!";
 var length = sign.length;
 var subtotal = length * 2.99;
 var shipping = 4.99;
-var total = subtotal + shipping;
+var total = Math.round(subtotal + shipping);
 
 document.getElementById("usersign").innerHTML= sign;
 document.getElementById("tiles").textContent = length;
 document.getElementById("subtotal").textContent = `£${subtotal}`;
 document.getElementById("shipping").textContent = `£${shipping}`;
-document.getElementById("total").textContent = `£${total}`;
+document.getElementById("total").textContent = `£${total}`
+*/
